c1fb6580959a6c5e85f9bbaee2b6ee98541e0fc2
mberti <marco.berti90@gmail.com>
2024-06-11 16:28:27 +0200

    formatting

--- src/main/kotlin/com/posadeus/app/configuration/infrastructure/AtpTourClientConfiguration.kt
+++ src/main/kotlin/com/posadeus/app/configuration/infrastructure/AtpTourClientConfiguration.kt
@@ -8,13 +8,16 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 
-@Configuration open class AtpTourClientConfiguration {
+@Configuration
+open class AtpTourClientConfiguration {
 
-  @Bean open fun atpTourGetClient(@Value("\${client.atp-tour.uri}") atpTourURI: String,
-                                  atpTourRestTemplate: RestTemplate): AtpTourClient =
+  @Bean
+  open fun atpTourGetClient(@Value("\${client.atp-tour.uri}") atpTourURI: String,
+                            atpTourRestTemplate: RestTemplate): AtpTourClient =
     RestAtpTourClient(atpTourURI, atpTourRestTemplate)
 
-  @Bean open fun atpTourRestTemplate(
+  @Bean
+  open fun atpTourRestTemplate(
     @Value("\${client.atp-tour.http.connection-request-timeout}") connectionRequestTimeout: Int,
     @Value("\${client.atp-tour.http.connection-timeout}") connectionTimeout: Int): RestTemplate {
 
