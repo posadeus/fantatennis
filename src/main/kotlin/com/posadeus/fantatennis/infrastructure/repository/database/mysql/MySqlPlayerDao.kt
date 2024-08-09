9492c31904465ddd884116db5b92e70f473941a0
mberti <marco.berti90@gmail.com>
2024-08-09 19:59:37 +0200

    feat: datasource configuration added and pom aligned

--- src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlPlayerDao.kt
+++ src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlPlayerDao.kt
@@ -2,5 +2,7 @@ package com.posadeus.fantatennis.infrastructure.repository.database.mysql
 
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.PlayerEntity
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MySqlPlayerDao : CrudRepository<PlayerEntity, String>
--- src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlTeamDao.kt
+++ src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlTeamDao.kt
@@ -3,7 +3,9 @@ package com.posadeus.fantatennis.infrastructure.repository.database.mysql
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.TeamEntity
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.TeamKeyEmbedded
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MySqlTeamDao : CrudRepository<TeamEntity, TeamKeyEmbedded> {
 
   fun findByIdTeamId(teamId: String): List<TeamEntity>
