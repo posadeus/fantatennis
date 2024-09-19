08a25836a76945bc9ace0053e9f5ffe6aca9269d
mberti <marco.berti90@gmail.com>
2024-09-19 12:26:13 +0200

    chore: PlayerController.kt removed

--- src/main/kotlin/com/posadeus/fantatennis/domain/service/player/PlayerService.kt
+++ src/main/kotlin/com/posadeus/fantatennis/domain/service/player/PlayerService.kt
@@ -1,13 +1,8 @@
 package com.posadeus.fantatennis.domain.service.player
 
 import com.posadeus.fantatennis.domain.model.DomainPlayer
-import com.posadeus.fantatennis.domain.model.ResultPlayer
 
 class PlayerService {
-
-  fun getPlayer(playerId: Int): ResultPlayer {
-    TODO("Not yet implemented")
-  }
 
   fun allPlayers(): Set<DomainPlayer> {
     TODO("Not yet implemented")
