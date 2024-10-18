8875bf8131693ee6bf5f7635286d3bf660c9717a
mberti <marco.berti90@gmail.com>
2024-10-18 10:48:26 +0200

    feat: MySqlFantaTeamsRepository error management implemented

--- src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlFantaTeamsRepository.kt
+++ src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlFantaTeamsRepository.kt
@@ -1,14 +1,19 @@
 package com.posadeus.fantatennis.infrastructure.repository.database.mysql
 
 import com.posadeus.fantatennis.domain.infrastructure.FantaTeamsRepository
-import com.posadeus.fantatennis.domain.model.FantaTeam
-import com.posadeus.fantatennis.domain.model.FantaTeamOk
+import com.posadeus.fantatennis.domain.model.*
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.dao.FantaTeamsDao
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.FantaTeamsEntity
 
 class MySqlFantaTeamsRepository(private val fantaTeamsDao: FantaTeamsDao) : FantaTeamsRepository {
 
   override fun createTeam(ownerId: String): FantaTeam =
-      fantaTeamsDao.save(FantaTeamsEntity(ownerId = ownerId))
-          .let { FantaTeamOk(id = it.teamId, ownerId = it.ownerId) }
+      try {
+        fantaTeamsDao.save(FantaTeamsEntity(ownerId = ownerId))
+            .let { FantaTeamOk(id = it.teamId, ownerId = it.ownerId) }
+      }
+      catch (e: Exception) {
+
+        FantaTeamError
+      }
 }
--- src/test/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlFantaTeamsRepositoryTest.kt
+++ src/test/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlFantaTeamsRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.posadeus.fantatennis.infrastructure.repository.database.mysql
 
 import com.posadeus.fantatennis.domain.infrastructure.FantaTeamsRepository
+import com.posadeus.fantatennis.domain.model.FantaTeamError
 import com.posadeus.fantatennis.domain.model.FantaTeamOk
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.dao.FantaTeamsDao
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.FantaTeamsEntity
@@ -24,6 +25,18 @@ class MySqlFantaTeamsRepositoryTest {
     val expected = FantaTeamOk(id = 1, ownerId = AN_OWNER_ID)
 
     every { dao.save(toCreateFantaTeamsEntity) } returns createdFantaTeamsEntity
+
+    assertThat(repository.createTeam(AN_OWNER_ID)).isEqualTo(expected)
+  }
+
+  @Test
+  fun `error during creation`() {
+
+    val toCreateFantaTeamsEntity = FantaTeamsEntity(ownerId = AN_OWNER_ID)
+
+    val expected = FantaTeamError
+
+    every { dao.save(toCreateFantaTeamsEntity) } throws Exception()
 
     assertThat(repository.createTeam(AN_OWNER_ID)).isEqualTo(expected)
   }
