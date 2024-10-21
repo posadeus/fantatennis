188042dfd25a0e8d47d14214a602c84836a8f7e9
mberti <marco.berti90@gmail.com>
2024-10-21 10:27:30 +0200

    refactor: readability improved on AddPlayersTeamService

--- src/main/kotlin/com/posadeus/fantatennis/domain/service/AddPlayersTeamService.kt
+++ src/main/kotlin/com/posadeus/fantatennis/domain/service/AddPlayersTeamService.kt
@@ -11,13 +11,16 @@ class AddPlayersTeamService(private val teamsRepository: TeamsRepository) {
   fun addPlayers(teamId: Int, playerIds: Set<String>): Team =
       when (val addPlayers = teamsRepository.addPlayers(teamId, playerIds)) {
 
-        is AddPlayersOk -> addPlayers.players
-            .map { TeamPlayerDto(fullName = it.fullName, fantaPoints = 0.0) }
-            .let { FoundTeam(team = TeamDto(players = it, totalScore = 0.0)) }
+        is AddPlayersOk -> toFoundTeam(addPlayers.players)
         is AddPlayersTeamNotFound -> TeamIdNotFoundTeam
         is PlayersNotFound -> ErrorTeam.also { LOGGER.error("Players not found: ${addPlayers.missingPlayersIds}") }
         is AddPlayersError -> ErrorTeam
       }
+
+  private fun toFoundTeam(players: Set<DomainPlayer>): FoundTeam =
+      players
+          .map { TeamPlayerDto(fullName = it.fullName, fantaPoints = 0.0) }
+          .let { FoundTeam(team = TeamDto(players = it, totalScore = 0.0)) }
 
   companion object {
 
