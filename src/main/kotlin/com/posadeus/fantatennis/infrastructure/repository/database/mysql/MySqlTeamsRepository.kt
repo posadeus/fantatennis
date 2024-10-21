c69ace5e0c11f86a5b7afc7396b814d92629a667
mberti <marco.berti90@gmail.com>
2024-10-21 16:27:06 +0200

    refactor: MySqlTeamsRepository readability improved

--- src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlTeamsRepository.kt
+++ src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/MySqlTeamsRepository.kt
@@ -6,8 +6,8 @@ import com.posadeus.fantatennis.domain.model.AddPlayers.InvalidAddPlayers.*
 import com.posadeus.fantatennis.domain.model.AddPlayers.ValidAddPlayers
 import com.posadeus.fantatennis.domain.model.DomainPlayer
 import com.posadeus.fantatennis.infrastructure.repository.database.mysql.dao.*
-import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.TeamsEntity
-import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.TeamsKeyEmbedded
+import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.*
+import kotlin.jvm.optionals.getOrNull
 
 class MySqlTeamsRepository(private val fantaTeamsDao: FantaTeamsDao,
                            private val playersDao: PlayersDao,
@@ -17,39 +17,48 @@ class MySqlTeamsRepository(private val fantaTeamsDao: FantaTeamsDao,
 
     try {
 
-      val fantaTeam = fantaTeamsDao.findById(teamId)
-
-      if (fantaTeam.isEmpty) return AddPlayersTeamNotFound
+      val fantaTeam = fantaTeamsDao.findById(teamId).getOrNull()
+                      ?: return AddPlayersTeamNotFound
 
       val players = playersDao.findAllById(playerIds)
 
       if (players.count() != playerIds.size) {
 
-        val missingPlayerIds = playerIds.filter { id -> id !in players.map { it.id } }.toSet()
-
-        return PlayersNotFound(missingPlayerIds = missingPlayerIds)
+        return PlayersNotFound(missingPlayerIds = missingPlayerIds(playerIds, players))
       }
 
-      val teamsEntities = players
-          .map {
-            TeamsEntity(id = TeamsKeyEmbedded(teamId = teamId, playerId = it.id),
-                        player = it,
-                        fantaTeam = fantaTeam.get())
-          }
+      return players
+          .map { toTeamsEntity(teamId, it, fantaTeam) }
           .toSet()
-
-      teamsDao.saveAll(teamsEntities)
-
-      return ValidAddPlayers(players = players.map {
-        DomainPlayer(id = it.id,
-                     atpId = it.atpTourId,
-                     fullName = it.fullName)
-      }
-          .toSet())
+          .let(::persist)
+          .map(::toDomainPlayer)
+          .toSet()
+          .let(::ValidAddPlayers)
     }
     catch (e: Exception) {
 
       return AddPlayersError
     }
   }
+
+  private fun persist(it: Set<TeamsEntity>): Iterable<TeamsEntity> =
+      teamsDao.saveAll(it)
+
+  private fun missingPlayerIds(playerIds: Set<String>,
+                               players: Iterable<PlayersEntity>) =
+      playerIds
+          .filter { id -> id !in players.map { it.id } }
+          .toSet()
+
+  private fun toDomainPlayer(teamsEntity: TeamsEntity) =
+      DomainPlayer(id = teamsEntity.player.id,
+                   atpId = teamsEntity.player.atpTourId,
+                   fullName = teamsEntity.player.fullName)
+
+  private fun toTeamsEntity(teamId: Int,
+                            playersEntity: PlayersEntity,
+                            fantaTeam: FantaTeamsEntity) =
+      TeamsEntity(id = TeamsKeyEmbedded(teamId = teamId, playerId = playersEntity.id),
+                  player = playersEntity,
+                  fantaTeam = fantaTeam)
 }
