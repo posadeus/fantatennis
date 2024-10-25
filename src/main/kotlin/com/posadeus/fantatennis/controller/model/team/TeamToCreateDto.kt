158c497dd37faaf8c9538ab1b33e8fa74e574b87
mberti <marco.berti90@gmail.com>
2024-10-25 09:46:29 +0200

    feat: Team creation endpoint accepts tournament param in the request

--- src/main/kotlin/com/posadeus/fantatennis/controller/model/team/TeamToCreateDto.kt
+++ src/main/kotlin/com/posadeus/fantatennis/controller/model/team/TeamToCreateDto.kt
@@ -6,10 +6,13 @@ import java.io.Serializable
 /**
  * 
  * @param ownerId 
+ * @param tournament 
  */
 data class TeamToCreateDto(
 
-    @get:JsonProperty("ownerId", required = true) val ownerId: String = ""
+    @get:JsonProperty("ownerId", required = true) val ownerId: String = "",
+
+    @get:JsonProperty("tournament", required = true) val tournament: TournamentCreationDto
     ) : Serializable{
 
     companion object {
