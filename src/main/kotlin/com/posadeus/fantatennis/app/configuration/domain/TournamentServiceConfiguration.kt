2a5481b34c6271ae077f862a61614c8a7eb13c1e
mberti <marco.berti90@gmail.com>
2025-01-15 09:42:13 +0100

    feat: TournamentServiceConfiguration: retrieveTournamentsService bean added

--- src/main/kotlin/com/posadeus/fantatennis/app/configuration/domain/TournamentServiceConfiguration.kt
+++ src/main/kotlin/com/posadeus/fantatennis/app/configuration/domain/TournamentServiceConfiguration.kt
@@ -1,8 +1,7 @@
 package com.posadeus.fantatennis.app.configuration.domain
 
 import com.posadeus.fantatennis.domain.infrastructure.FantaTournamentsRepository
-import com.posadeus.fantatennis.domain.service.tournament.CreateTournamentService
-import com.posadeus.fantatennis.domain.service.tournament.RetrieveTournamentService
+import com.posadeus.fantatennis.domain.service.tournament.*
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -16,4 +15,8 @@ class TournamentServiceConfiguration {
   @Bean
   fun retrieveTournamentService(fantaTournamentsRepository: FantaTournamentsRepository): RetrieveTournamentService =
       RetrieveTournamentService(fantaTournamentsRepository)
+
+  @Bean
+  fun retrieveTournamentsService(fantaTournamentsRepository: FantaTournamentsRepository): RetrieveTournamentsService =
+      RetrieveTournamentsService(fantaTournamentsRepository)
 }
