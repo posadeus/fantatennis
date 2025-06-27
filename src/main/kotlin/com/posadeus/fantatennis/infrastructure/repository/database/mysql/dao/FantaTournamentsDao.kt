878e9c44312793166c33426d7b873a6443eb22bb
mberti <marco.berti90@gmail.com>
2025-06-27 10:34:53 +0200

    refactor: cleanup

--- src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/mysql/dao/FantaTournamentsDao.kt
+++
@@ -1,8 +0,0 @@
-package com.posadeus.fantatennis.infrastructure.repository.database.mysql.dao
-
-import com.posadeus.fantatennis.infrastructure.repository.database.mysql.model.FantaTournamentsEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface FantaTournamentsDao : JpaRepository<FantaTournamentsEntity, Int>
