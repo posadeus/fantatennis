4946016b3a9185761ea5cfd9a7d246797503aad8
mberti <marco.berti90@gmail.com>
2025-07-25 13:29:34 +0200

    chore: cleanup

--- src/main/kotlin/com/posadeus/fantatennis/app/Application.kt
+++ src/main/kotlin/com/posadeus/fantatennis/app/Application.kt
@@ -1,15 +1,11 @@
 package com.posadeus.fantatennis.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EntityScan(basePackages = ["com.posadeus.fantatennis.infrastructure.repository.database.mysql.model"])
-@EnableJpaRepositories(basePackages = ["com.posadeus.fantatennis.infrastructure.repository.database"])
 class Application
 
 fun main(args: Array<String>) {
