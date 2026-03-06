a3056c12ffe1358f79c16e074661a15ba8083b6d
mberti <marco.berti90@gmail.com>
2026-03-06 11:19:28 +0100

    refactor: restructure SQL queries in JdbcRetrieveFantaTournamentResultsRepository for clarity and organization

--- src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/jdbc/JdbcRetrieveFantaTournamentResultsRepository.kt
+++ src/main/kotlin/com/posadeus/fantatennis/infrastructure/repository/database/jdbc/JdbcRetrieveFantaTournamentResultsRepository.kt
@@ -112,56 +112,71 @@ class JdbcRetrieveFantaTournamentResultsRepository(private val namedParameterJdb
         TOTAL_SCORE DESC;
     """.trimIndent()
 
-//    FantaTournamentDao.retrieveBy(fantaTournamentId)
+//    SELECT
+//        playerStandsForTeam.TEAM_ID,
+//        ft2.OWNER_ID,
+//        playerStandsForTeam.PLAYER_ID,
+//        playerPointsByTournament.FULL_NAME,
+//        SUM(playerPointsByTournament.FANTA_POINTS) AS TOTAL_SCORE
+//      FROM
+//      (
+// PlayerPointsDao.retrieveByTournamentYear(year)
+//        SELECT
+//          pp.PLAYER_ID,
+//          pl.FULL_NAME,
+//          pp.TOURNAMENT_ID,
+//          pp.FANTA_POINTS
+//        FROM PLAYERS_POINTS pp
+//        LEFT JOIN PLAYERS pl ON pl.PLAYER_ID = pp.PLAYER_ID,
+//        (
+// FantaTournamentDao.retrieveBy(fantaTournamentId)
 //          SELECT
 //            ft.STARTING_TOURNAMENT,
 //            ft.ENDING_TOURNAMENT,
 //            ft.TOURNAMENT_YEAR
 //          FROM FANTA_TOURNAMENTS ft
 //          WHERE ft.FANTA_TOURNAMENT_ID = :fantaTournamentId
-
-//    FantaTournamentTeamDao.retrieveBy(fantaTournamentId)
+//--------------- FantaTournamentDao
+//        ) AS fttt
+//        WHERE pp.TOURNAMENT_YEAR = fttt.TOURNAMENT_YEAR
+//        AND pp.TOURNAMENT_ID >= fttt.STARTING_TOURNAMENT
+//        AND pp.TOURNAMENT_ID <= fttt.ENDING_TOURNAMENT
+//        ORDER BY
+//          pp.PLAYER_ID ASC,
+//          pp.TOURNAMENT_ID ASC
+//      ) as playerPointsByTournament,
+//--------------- PlayerPointsDao
+//      (
+// TeamDao.retrieveBy(teamIds)
+//        SELECT
+//          tm.TEAM_ID,
+//          tm.PLAYER_ID,
+//          tm.STARTING_TOURNAMENT,
+//          tm.ENDING_TOURNAMENT
+//        FROM TEAMS tm
+//        WHERE tm.TEAM_ID IN (
+// FantaTournamentTeamDao.retrieveBy(fantaTournamentId)
 //          SELECT ftts.TEAM_ID
 //          FROM FANTA_TOURNAMENTS_TEAMS as ftts
 //          WHERE ftts.FANTA_TOURNAMENT_ID = :fantaTournamentId
-
-//    PlayerPointsDao.retrieveByTournamentYear(year)
-//          SELECT
-//                pp.PLAYER_ID,
-//                pl.FULL_NAME,
-//                pp.TOURNAMENT_ID,
-//                pp.FANTA_POINTS
-//              FROM PLAYERS_POINTS pp
-//              LEFT JOIN PLAYERS pl ON pl.PLAYER_ID = pp.PLAYER_ID,
-//              (
-//                SELECT
-//                  ft.STARTING_TOURNAMENT,
-//                  ft.ENDING_TOURNAMENT,
-//                  ft.TOURNAMENT_YEAR
-//                FROM FANTA_TOURNAMENTS ft
-//                WHERE ft.FANTA_TOURNAMENT_ID = :fantaTournamentId
-//              ) AS fttt
-//              WHERE pp.TOURNAMENT_YEAR = fttt.TOURNAMENT_YEAR
-//              AND pp.TOURNAMENT_ID >= fttt.STARTING_TOURNAMENT
-//              AND pp.TOURNAMENT_ID <= fttt.ENDING_TOURNAMENT
-//              ORDER BY
-//                pp.PLAYER_ID ASC,
-//                pp.TOURNAMENT_ID ASC
-//            ) as playerPointsByTournament,
-
-//    TeamDao.retrieveBy(teamIds)
-//          SELECT
-//                tm.TEAM_ID,
-//                tm.PLAYER_ID,
-//                tm.STARTING_TOURNAMENT,
-//                tm.ENDING_TOURNAMENT
-//              FROM TEAMS tm
-//              WHERE tm.TEAM_ID IN (
-//                SELECT ftts.TEAM_ID
-//                FROM FANTA_TOURNAMENTS_TEAMS as ftts
-//                WHERE ftts.FANTA_TOURNAMENT_ID = :fantaTournamentId
-//              ) ORDER BY tm.TEAM_ID
-//            ) as playerStandsForTeam
+//-------------- FantaTournamentTeamDao
+//        ) ORDER BY tm.TEAM_ID
+//      ) as playerStandsForTeam
+//-------------- TeamDao
+//      LEFT JOIN FANTA_TEAMS ft2 ON playerStandsForTeam.TEAM_ID = ft2.TEAM_ID
+//      WHERE playerStandsForTeam.PLAYER_ID = playerPointsByTournament.PLAYER_ID
+//      AND
+//      (
+//        IF (playerStandsForTeam.ENDING_TOURNAMENT IS NULL,
+//          playerPointsByTournament.TOURNAMENT_ID >= playerStandsForTeam.STARTING_TOURNAMENT,
+//          playerPointsByTournament.TOURNAMENT_ID BETWEEN playerStandsForTeam.STARTING_TOURNAMENT and playerStandsForTeam.ENDING_TOURNAMENT)
+//      )
+//      GROUP BY
+//        playerStandsForTeam.PLAYER_ID,
+//        playerStandsForTeam.TEAM_ID
+//      ORDER BY
+//        TEAM_ID ASC,
+//        TOTAL_SCORE DESC;
 
     private val LOGGER = LoggerFactory.getLogger(JdbcRetrieveFantaTournamentResultsRepository::class.java)
   }
