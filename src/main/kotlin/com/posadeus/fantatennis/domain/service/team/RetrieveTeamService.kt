55cbe12141817c844f2643f11c12e9ece520232c
mberti <marco.berti90@gmail.com>
2026-05-22 11:42:10 +0200

    refactor: map InvalidFantaTournament to ErrorTeam in RetrieveTeamService

--- src/main/kotlin/com/posadeus/fantatennis/domain/service/team/RetrieveTeamService.kt
+++ src/main/kotlin/com/posadeus/fantatennis/domain/service/team/RetrieveTeamService.kt
@@ -4,14 +4,22 @@ import com.posadeus.fantatennis.domain.infrastructure.*
 import com.posadeus.fantatennis.domain.model.*
 import com.posadeus.fantatennis.domain.model.DomainTeam.FoundDomainTeam
 import com.posadeus.fantatennis.domain.model.DomainTeam.NotFoundDomainTeam
+import com.posadeus.fantatennis.domain.model.FantaTournament.InvalidFantaTournament
+import com.posadeus.fantatennis.domain.model.FantaTournament.ValidFantaTournament
 
 class RetrieveTeamService(private val retrieveFantaTeamRepository: RetrieveFantaTeamRepository,
                           private val retrieveFantaTournamentsRepository: RetrieveFantaTournamentsRepository,
                           private val retrievePlayersPointsRepository: RetrievePlayersPointsRepository) {
 
   fun retrieve(teamId: Int): Team =
-      when (retrieveFantaTeamRepository.retrieveByTeamId(teamId)) {
+      when (val team = retrieveFantaTeamRepository.retrieveByTeamId(teamId)) {
+
         is NotFoundDomainTeam -> TeamIdNotFoundTeam
-        is FoundDomainTeam -> ErrorTeam
+        is FoundDomainTeam ->
+          when (retrieveFantaTournamentsRepository.retrieveBy(team.fantaTournamentId)) {
+
+            is InvalidFantaTournament -> ErrorTeam
+            is ValidFantaTournament -> ErrorTeam
+          }
       }
 }
--- src/test/kotlin/com/posadeus/fantatennis/domain/service/team/RetrieveTeamServiceTest.kt
+++ src/test/kotlin/com/posadeus/fantatennis/domain/service/team/RetrieveTeamServiceTest.kt
@@ -1,7 +1,10 @@
 package com.posadeus.fantatennis.domain.service.team
 
 import com.posadeus.fantatennis.domain.infrastructure.*
+import com.posadeus.fantatennis.domain.model.DomainTeam.FoundDomainTeam
 import com.posadeus.fantatennis.domain.model.DomainTeam.NotFoundDomainTeam
+import com.posadeus.fantatennis.domain.model.ErrorTeam
+import com.posadeus.fantatennis.domain.model.FantaTournament.InvalidFantaTournament
 import com.posadeus.fantatennis.domain.model.TeamIdNotFoundTeam
 import io.mockk.every
 import io.mockk.mockk
@@ -26,8 +29,24 @@ class RetrieveTeamServiceTest {
     assertThat(service.retrieve(A_TEAM_ID)).isEqualTo(TeamIdNotFoundTeam)
   }
 
+  @Test
+  fun `error when retrieveBy returns InvalidFantaTournament`() {
+
+    val foundDomainTeam = FoundDomainTeam(teamId = A_TEAM_ID,
+                                          ownerId = AN_OWNER_ID,
+                                          fantaTournamentId = A_FANTA_TOURNAMENT_ID,
+                                          players = emptyMap())
+
+    every { retrieveFantaTeamRepository.retrieveByTeamId(A_TEAM_ID) } returns foundDomainTeam
+    every { retrieveFantaTournamentsRepository.retrieveBy(A_FANTA_TOURNAMENT_ID) } returns InvalidFantaTournament
+
+    assertThat(service.retrieve(A_TEAM_ID)).isEqualTo(ErrorTeam)
+  }
+
   companion object {
 
     private const val A_TEAM_ID = 1
+    private const val AN_OWNER_ID = "AN_OWNER_ID"
+    private const val A_FANTA_TOURNAMENT_ID = 42
   }
 }
